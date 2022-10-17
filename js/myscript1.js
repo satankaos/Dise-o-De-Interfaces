e9a720d3e3999685a7445bc96608d10d8c07660b
gamiz <71780447+satankaos@users.noreply.github.com>
2022-10-17 20:08:34 +0200

    exito

---
+++ js/myscript1.js
@@ -0,0 +1,41 @@
+//Sort
+//var testArray = [10, 1,2,3,5,9,24, 22, 45,32];
+var testArray = "10, 1,2,3,5,9,24, 22, 45,32";
+// testArray.sort ();
+// console.log ( testArray);
+// testArray.sort ()
+// function sortNumber (a, b){
+// return a-b;
+// }
+//testArray.reverse()
+//var testArray = testArray.split(",")
+//var testJoin = testArray.join(".")
+//Filter
+/*var testArray = [10, 2, 4,3,5, 91];
+var pairnumber = testArray. filter( (num)=>{
+return num % 2 === 0;
+})
+console.log (pairnumber) ;
+//console.log(testJoin);*/
+/*// Map
+//se le aplica algo 
+var testArray = [1,2,3,5,7];
+console.log(testArray);
+var testArray2 = testArray. map( function (num){
+return num*num;//esto es lo que se aplica
+})
+console.log ( testArray2);*/
+//Reduce I
+/*//Callbacks
+setTimeout ( function () {
+alert("Hello world");
+}, 2500);*/
+/*/// Closures
+document.getElementById ("btn99").addEventListener ("click", ()=>{
+console.log("Click detected" );
+})
+(function (){
+document.getElementById ("btn99").addEventListener ("click", ()=>{
+    console.log("Click detected" );})
+})();*/
+
